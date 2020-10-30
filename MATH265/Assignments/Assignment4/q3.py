f888b1c975f9825a9b5c32d74577f04ba08f960e
stanley-zheng <stanleyjzheng@gmail.com>
2020-10-30 10:31:05 -0600

    Complete MATH265 final and begin MATH270 Assignment 4

--- MATH265/Assignments/Assignment4/q3.py
+++ MATH265/Assignments/Assignment4/q3.py
@@ -13,12 +13,14 @@ y1 = sympy.lambdify(x, y1, 'numpy')
 plt.grid(True)
 plt.axhline(y=0, lw=1, color='black')
 plt.axvline(x=0, lw=1, color='black')
-plt.xticks(np.arange(-10, 10, 1.0))
-plt.yticks(np.arange(-10, 10, 1.0))
+plt.xticks(np.arange(-100, 100, 1.0))
+plt.yticks(np.arange(-100, 100, 1.0))
 
 
 plt.plot(x1,y1(x1), color='black', linewidth=1)
 
+plt.ylim(-10, 10)
+plt.xlim(-10, 10)
 # plt.plot(3, 9, marker='.', color='black')
 
 plt.show()
